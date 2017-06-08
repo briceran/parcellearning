61aca3e7619fe02921e8d1a010b37635520bad77
Kristian Eschenburg <keschenb@uw.edu>
2017-06-07 17:17:17 -0700

    fixed some errors

--- parcellearning/regionalizationMethods.py
+++ parcellearning/regionalizationMethods.py
@@ -113,66 +113,41 @@ def labelLayers(lab,labelIndices,surfAdj,borderIndices):
         
         # get subgraph nodes
         sg_nodes = subGraph.nodes()
-        # get subgraph border indices
-        sg_borders = list(set(sg_nodes).intersection(borderIndices))
-        # get subgraph internal indices
-        sg_internal = list(set(sg_nodes).intersection(internalNodes))
         
-        # if the subGraph internal nodes are a list
-        if isinstance(sg_internal,list):
-            for i,n in enumerate(sg_internal):
+        # make sure subgraph has more than a single component
+        if len(sg_nodes) > 1:
             
-                # if the border vertices are a list
-                if isinstance(sg_borders,list):
-                    for b in sg_borders:
-                        if nx.has_path(subGraph,source=n,target=b):
-                            sg_nb = nx.nx.shortest_path_length(subGraph,
-                                                               source=n,
-                                                               target=b)
-                        else:
-                            sg_nb = None
-                            
-                        distances[n].append(sg_nb)
-                # if the border vertices are an integer
-                else:
-                    if nx.has_path(subGraph,source=n,target=sg_borders):
-                            sg_nb = nx.nx.shortest_path_length(subGraph,
-                                                               source=n,
-                                                               target=sg_borders)
-                    else:
-                        sg_nb = None
-                    distances[n].append(sg_nb)
+            # get subgraph border indices
+            sg_border = list(set(sg_nodes).intersection(borderIndices))
+            # get subgraph internal indices
+            sg_internal = list(set(sg_nodes).intersection(internalNodes))
+            
+            # if single elements, convert to lists
+            if isinstance(sg_border,int):
+                sg_border = list([sg_border])
+            
+            if isinstance(sg_internal,int):
+                sg_internal = list([sg_internal])
                 
-        # if the subGraph internal nodes are a single integer
-        else:
-            # if the border vertices are a list
-            if isinstance(sg_borders,list):
-                for b in sg_borders:
-                    if nx.has_path(subGraph,source=sg_internal,target=b):
-                            sg_nb = nx.nx.shortest_path_length(subGraph,
-                                                               source=sg_internal,
-                                                               target=b)
+            for i,n in enumerate(sg_internal):
+                # iterate over border vertices
+                for b in sg_border:
+                    if nx.has_path(subGraph,source=n,target=b):
+                        sg_nb = nx.shortest_path_length(subGraph,source=n,
+                                                        target=b)
                     else:
                         sg_nb = None
                     distances[n].append(sg_nb)
-                    
-            # if the border vertices are an integer
-            else:
-                if nx.has_path(subGraph,source=sg_internal,target=sg_borders):
-                    sg_nb = nx.shortest_path_length(subGraph,
-                                                    source=sg_internal,
-                                                    target=sg_borders)
-                else:
-                    sg_nb = None
-                distances[n].append(sg_nb)
-                                
-    for n in distances.keys():
-        if distances[n]:
-            if isinstance(distances[n],list):
-                distances[n] = min(distances[n])
+    
+    for key in distances.keys():
+        if distances[key]:
+            if isinstance(distances[key],list):
+                distances[key] = min(distances[key])
+        else:
+            distances[key] = None
 
     layered = {k: [] for k in set(distances.values())}
-    
+
     for vertex in distances.keys():
         dist = distances[vertex]
         layered[dist].append(vertex)
@@ -221,7 +196,7 @@ def parseColorLookUpFile(lookupTable):
     
     return parsedColors
 
-def shiftColor(rgb,mag=20):
+def shiftColor(rgb,mag=30):
     
     """
     Method to adjust rgba slightly slightly.
@@ -232,7 +207,6 @@ def shiftColor(rgb,mag=20):
     for i in np.arange(3):
         
         r = np.random.choice([-1,1])
-        print(r)
         m = (1*r*mag)
         
         adj = rgb[i]+m
@@ -245,5 +219,98 @@ def shiftColor(rgb,mag=20):
         rgb_adj.append(adj)
     
     return rgb_adj
+
+def neighborhoodErrorMap(core,labelAdjacency,truthLabFile,
+                         predLabFile,labelLookup,outputColorMap):
+    
+    """
+    Method to visualize the results of a prediction map, focusing in on a 
+    spefic core label.
+    
+    Parameters:
+    - - - - -
+        core : region of interest
+        labelAdjacency : label adjacency list
+        truthLabFile : ground truth label file
+        predLabFile : predicted label file
+        labelLookup : label color lookup table
+    """
+    
+    # load files
+    labAdj = ld.loadPick(labelAdjacency)
+    truth = ld.loadGii(truthLabFile,0)
+    pred = ld.loadGii(predLabFile,0)
+    
+    # initialize new color map file
+    color_file = open(outputColorMap,"w")
+    
+    # extract current colors from colormap
+    parsedColors = parseColorLookUpFile(labelLookup)
+    
+    # get labels that neighbor core label
+    neighbors = labAdj[core]
+    # get indices of core label in true map
+    truthInds = np.where(truth == core)[0]
+    
+    # initialize new map
+    visualizeMap = np.zeros((truth.shape))
+    visualizeMap[truthInds] = core
+    
+    trueColors = [255,255,255]
+    trueColors = map(str,trueColors)
+    
+    trueName = 'Label {}'.format(core)
+    trueRGBA = '{} {} {}\n'.format(core,' '.join(trueColors),255)
+    
+    trueStr = '\n'.join([trueName,trueRGBA])
+    color_file.writelines(trueStr)
+    
+    # get predicted label values existing at truthInds
+    predLabelsTruth = pred[truthInds]
+    print('confused labels: ',set(predLabelsTruth))
+
+    for n in neighbors:
+        
+        originalLabel = n
+        
+        # get color code for label, and adjust
+        colorCode = parsedColors[n]
+        adjColors = shiftColor(colorCode,mag=20)
+        adjColors = map(str,adjColors)
+        
+        # convert original code to string
+        originalColorStr = map(str,colorCode)
+        
+        n_inds = np.where(truth == originalLabel)[0]
+        visualizeMap[n_inds] = originalLabel
+        
+        # write original label data to text file
+        originalLabelName = 'Label {}'.format(originalLabel)
+        originalRGBA = '{} {} {}\n'.format(originalLabel,
+                        ' '.join(originalColorStr),255)
+        originalStr = '\n'.join([originalLabelName,originalRGBA])
+        
+        color_file.writelines(originalStr)
+        
+        # create new label, outside original bounds
+        newLabel = originalLabel+180
+        
+        # write new label data to text file
+        label_name = 'Label {}'.format(newLabel)
+        labelRGBA  = '{} {} {}\n'.format(newLabel,' '.join(adjColors),255)
+        newStr = '\n'.join([label_name,labelRGBA])
+        
+        color_file.writelines(newStr)
+        
+        n_inds = np.where(predLabelsTruth == n)[0]
+        visualizeMap[truthInds[n_inds]] = newLabel
+    
+    color_file.close()
+    
+    
+    
+    return visualizeMap
+        
+        
         
         
