eaf85fe376967a58a01814615dce221f0d3805fd
Kristian Eschenburg <keschenb@uw.edu>
2017-07-25 15:26:13 -0700

    fixed midlines

--- bin/processMidlines.py
+++ bin/processMidlines.py
@@ -11,29 +11,40 @@ import os
 import numpy as np
 
 def processMidlines(subjectList,dataDir,hemi):
+
+    hemis = ['Left','Right']
     
     with open(subjectList,'r') as inFile:
         subjects = inFile.readlines()
     subjects = [x.strip() for x in subjects]
     
     for s in subjects:
-        subjDir = dataDir + s + '/Resting_State/'
-        subjRest = subjDir + 'rfMRI_Z-Trans_merged_CORTEX_RIGHT.mat'
-        outMids = subjDir + s + '.' + hemi + '.Midline_Indices.mat'
-        
-        if os.path.isfile(subjRest):
-            S = sio.loadmat(subjRest)
-            rsData = S['rest']
-            
-            np.sum(np.abs(rsData),axis=1)
-            sm = np.sum(np.abs(rsData),axis=1);
-            
-            mids = np.where(sm == 0)[0]
-            
-            m = {}
-            m['mids'] = mids
-            
-            sio.savemat(outMids,m)
+
+        for h in hemis:
+            if h == 'Left':
+                H = 'LEFT'
+                HH = 'L'
+            else:
+                H = 'RIGHT'
+                HH = 'R'
+
+                subjDir = '{}{}/RestingState/'.format(dataDir,s)
+                subjRest = '{}rfMRI_Z-Trans_merged_CORTEX_{}.mat'.format(subjDir,H)
+                outMids = '{}{}.{}.Midline_Indices.mat'.format(subjDir,s,HH);
+                
+                if os.path.isfile(subjRest):
+                    S = sio.loadmat(subjRest)
+                    rsData = S['rest']
+                    
+                    np.sum(np.abs(rsData),axis=1)
+                    sm = np.sum(np.abs(rsData),axis=1);
+                    
+                    mids = np.where(sm == 0)[0]
+                    
+                    m = {}
+                    m['mids'] = mids
+                    
+                    sio.savemat(outMids,m)
         
 if __name__=="__main__":
     
