e53c2783d86f4e930d7c4c113ea4fbcc8627ae87
Kristian Eschenburg <keschenb@uw.edu>
2017-08-05 19:09:14 -0700

    reg

--- parcellearning/regionalizationMethods.py
+++ parcellearning/regionalizationMethods.py
@@ -312,7 +312,7 @@ def labelLayers(lab,labelIndices,surfAdj,borderIndices):
                     distances[v] = int(np.asarray(minDist[k]))
 
     print 'layers'
-    layered = {k: [] for k in set(distances.values())}
+    layered = {k: [] for k in set(np.squeeze(distances.values()))}
     
     for vertex in distances.keys():
         dist = distances[vertex]
