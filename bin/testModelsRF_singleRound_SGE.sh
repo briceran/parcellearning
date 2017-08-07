6964a3fd60ceae9838858399b46812f4d6ac20d5
kristianeschenburg <keschenb@uw.edu>
2017-08-06 19:33:55 -0700

    .

--- bin/testModelsRF_singleRound_SGE.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-round=$1
-
-PYTHON=/project_space/parcellation/Software/anaconda2/bin/python
-binDir=/mnt/parcellator/parcellation/GitHub/parcellearning/bin/
-script=testModelsRF_SingleRound.py
-
-${PYTHON} ${binDir}${script} -r ${round}
