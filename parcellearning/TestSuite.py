d11b6cbd02ba05dc158034982215eb8c2342aa33
Kristian Eschenburg <keschenb@uw.edu>
2017-09-20 18:39:19 -0700

    .

--- parcellearning/TestSuite.py
+++ parcellearning/TestSuite.py
@@ -259,7 +259,7 @@ if __name__ == "__main__":
                        batch,rate)
     
     tRt = [1,2]
-    freqs = [0,1,2,3,4]
+    freqs = [0,1,2,2.5,3,4]
     layers = [2,3]
     nodes = [25,50]
     hemi = ['L','R']
