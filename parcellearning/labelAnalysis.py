b62662e6500a043d906d1ed23e60a8aafdf020d9
Kristian Eschenburg <keschenb@uw.edu>
2018-01-14 17:21:49 -0800

    .

--- parcellearning/labelAnalysis.py
+++ parcellearning/labelAnalysis.py
@@ -16,7 +16,7 @@ import loaded as ld
 import networkx as nx
 import numpy as np
 
-def labelErrorDistances(surfaceAdjFile,trueFile,midlineFile,predictedFile):
+def labelErrorDistances(surfaceAdjFile,trueFile,midlineFile,predictedFile,maxValue):
     
     """
     Compute how far away the misclassified label is from the predicted label.
@@ -31,27 +31,34 @@ def labelErrorDistances(surfaceAdjFile,trueFile,midlineFile,predictedFile):
         predictedFile : predicted cortical parcellation map file
     """
     
-    
+    reIndexed = np.zeros((maxValue+1,maxValue+1))
+
     pred = ld.loadGii(predictedFile,0)
     true = ld.loadGii(trueFile,0)
     errors = np.where(true != pred)[0]
-    errorDistance = np.zeros((len(errors),))
     
     L = aj.LabelAdjacency(trueFile,surfaceAdjFile,midlineFile)
     L.generate_adjList()
     
     labelAdj = L.adj_list
     
+    # create graph of label adjacency
     GL = nx.from_dict_of_lists(labelAdj)
-    apsp = nx.all_pairs_shortest_path_length(GL)
-        
-    for j,e in enumerate(errors):
-        trueLabel = true[e]
-        predLabel = pred[e]
-
-        if predLabel in set(true):
-            errorDistance[j] = apsp[trueLabel][int(predLabel)]
-        else:
-            errorDistance[j] = np.nan
     
+    # get matrix of pairwise shortest paths between all labels
+    apsp = nx.floyd_warshall_numpy(GL)
+    res_apsp = np.asarray(np.reshape(apsp,np.product(apsp.shape)).T)
+    unraveled = np.unravel_index(np.arange(len(res_apsp)),apsp.shape)
+    
+    newInds = (np.asarray(GL.nodes())[unraveled[0]],
+               np.asarray(GL.nodes())[unraveled[1]])
+    
+    nans = list(set(np.arange(maxValue+1)).difference(set(GL.nodes())))
+    
+    reIndexed[newInds] = apsp[unraveled]
+    reIndexed[nans,:] = np.nan
+    reIndexed[:,nans] = np.nan
+    
+    errorDistance = reIndexed[true[errors],pred[errors]]
+
     return errorDistance
